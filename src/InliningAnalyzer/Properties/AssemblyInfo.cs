b11608130b77f580c32b4fd0bbad4b07e35b21ef
Stephan Zehetner <stephan.zehetner@gmail.com>
2017-10-31 21:44:57 +0100

    updated version to 0.6.2

--- src/InliningAnalyzer/Properties/AssemblyInfo.cs
+++ src/InliningAnalyzer/Properties/AssemblyInfo.cs
@@ -32,5 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(ProjectVersion.VersionNumber)]
+[assembly: AssemblyFileVersion(ProjectVersion.VersionNumber)]
+
+public static class ProjectVersion
+{
+    public const string VersionNumber = "0.6.2.0";
+}
--- src/Tests/Properties/AssemblyInfo.cs
+++ src/Tests/Properties/AssemblyInfo.cs
@@ -15,6 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("8c146611-aa24-46da-9dab-a9c1495f1e79")]
 
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(ProjectVersion.VersionNumber)]
+[assembly: AssemblyFileVersion(ProjectVersion.VersionNumber)]
