5f0bef3d01cd8dad36ce1432d8b55589771e111a
Stephan Zehetner <stephan.zehetner@gmail.com>
2018-05-07 23:03:31 +0200

    updated version to 0.7.0

--- src/InliningAnalyzer/Properties/AssemblyInfo.cs
+++ src/InliningAnalyzer/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 
 public static class ProjectVersion
 {
-    public const string VersionNumber = "0.6.5.0";
+    public const string VersionNumber = "0.7.0.0";
 }
